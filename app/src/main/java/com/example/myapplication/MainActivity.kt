33f6f77fbde10c7be243beae7b68d4bff84e6a5a
elliotchinglee <chinglee.tang@hexainitiative.com>
2021-03-09 11:56:28 +0800

    update

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SimpleAdapter
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerViewAccessibilityDelegate
-import kotlinx.android.synthetic.main.item_simple.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +14,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var rv: RecyclerView = findViewById(R.id.rv_user_address)
 
-        val a = arrayListOf<String>()
-        for(i in 0 until 6){
-            a.add(i.toString())
+        val a = arrayListOf<Obbb>()
+        for (i in 0 until 2) {
+            a.add(Obbb("var A var A var A var A var A $i", "var B var B var B var B $i"))
         }
         rv.adapter = AaAdapter(a)
         rv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
@@ -29,15 +24,15 @@ class MainActivity : AppCompatActivity() {
 
 
     class AaAdapter(
-            var items: ArrayList<String>
-    ) :RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var items: ArrayList<Obbb>
+    ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
             return AaViewHolder(
-                        LayoutInflater.from(parent.context)
-                                .inflate(R.layout.item_simple, parent, false)
-                )
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_simple, parent, false)
+            )
         }
 
         override fun getItemCount(): Int {
@@ -45,11 +40,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            holder.itemView.text.text = items[position]
+//            holder.itemView.t1?.text = items[position].varA
+//            holder.itemView.t2?.text = items[position].varB
+//            if (position == 1) {
+//                holder.itemView.ll?.visibility = View.GONE
+//            }
         }
 
         inner class AaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         }
 
     }
+
 }
+
+data class Obbb(
+    val varA: String,
+    val varB: String
+)
